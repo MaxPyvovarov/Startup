3a8e5c9115ff9d6509c2bbbed949eb6316c93587
Head4oT <max.pivovar@gmail.com>
2021-12-09 18:53:07 +0200

    Header section + responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 window.$ = window.jQuery = require('jquery');
+
+$(document).ready(function () {
+    //Mobile menu
+    $('.menu-button').on('click', function() {
+        $('.navigation-mobile').addClass('active');
+    })
+    $('.close-menu').on('click', function() {
+        $('.navigation-mobile').removeClass('active');
+    })
+});

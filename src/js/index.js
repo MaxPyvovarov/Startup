959f12aa9707f36ecf86a98bb47763dbe0e81579
maks159top.info <maks159top099@gmail.ru>
2021-12-10 20:18:16 +0200

    Lesson13

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,21 @@ $(document).ready(function () {
     $('.close-menu').on('click', function() {
         $('.navigation-mobile').removeClass('active');
     })
+
+    //Smooth scroll
+    $('a').on("click", function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function () {
+                window.location.hash = hash;
+            });
+        }
+        // event.preventDefault();
+        // var id  = $(this).attr('href'),
+        // top = $(id).offset().top;
+        // $('body,html').animate({scrollTop: top}, 700);
+    });
 });

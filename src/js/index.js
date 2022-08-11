0da81353e09752945dc6872a46bae8db68dd74e1
MaxPyvovarov <max.pivovar@gmail.com>
2022-08-11 18:58:28 +0300

    jQuery removed and other minor improvements

--- src/js/index.js
+++ src/js/index.js
@@ -1,93 +1,124 @@
-window.$ = window.jQuery = require('jquery');
-import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
-Swiper.use([ Autoplay, Navigation, Pagination]);
+import Swiper, {Autoplay, Navigation, Pagination} from 'swiper';
+Swiper.use([Autoplay, Navigation, Pagination]);
 
-$(document).ready(function () {
-    //Mobile menu
-    $('.menu-button').on('click', function() {
-        $('.navigation-mobile').addClass('active');
-    })
-    $('.close-menu').on('click', function() {
-        $('.navigation-mobile').removeClass('active');
-    })
+window.addEventListener('DOMContentLoaded', () => {
+	//Mobile menu
+	const menuButton = document.querySelector('.menu-button'),
+		closeMenuButton = document.querySelector('.close-menu'),
+		navMobile = document.querySelector('.navigation-mobile');
 
-    //Anchor links
-    $('a').on("click", function (event) {
-        if (this.hash !== '') {
-            event.preventDefault();
-            const hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function () {
-                window.location.hash = hash;
-            });
-        }
-    });
+	menuButton.addEventListener('click', () => {
+		navMobile.classList.add('active');
+	});
 
-    //About swiper
+	closeMenuButton.addEventListener('click', () => {
+		navMobile.classList.remove('active');
+	});
 
-    const swiper = new Swiper('#about .swiper', {
-        loop: true,
-        autoplay: {
-            delay: 5000,
-        },
-        slidesPerView: 1,
-        spaceBetween: 30,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            600: {
-                slidesPerView: 2,
-                spaceBetween: 30
-              },
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 30
-              },
-            
-            1224: {
-              slidesPerView: 4,
-              spaceBetween: 30
-            },
-        },
-    });
+	//Sticky header
+	const headerHeight = document.querySelector('.wrapper-fixed').offsetHeight,
+		bannerHeight = document.querySelector('#header').offsetHeight,
+		wrapperFixed = document.querySelector('.wrapper-fixed');
 
-    //Clients swiper
-    const clients_swiper = new Swiper('.clients__swiper', {
-        loop: true,
-        autoplay: {
-            delay: 5000,
-        },
-        slidesPerView: 1,
-        spaceBetween: 30,
-        autoHeight: true,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-            autoheight: true
+	window.addEventListener('scroll', () => {
+		if (window.scrollY >= bannerHeight - headerHeight) {
+			wrapperFixed.classList.add('sticky');
+			console.log('added sticky');
+		} else {
+			wrapperFixed.classList.remove('sticky');
+		}
+	});
 
-        },
-    });
+	//About swiper
 
-     //Tabs
-    $('.tabs__button').on('click', function () {
-        $(".tabs .tabs__button").removeClass("active").eq($(this).index()).addClass("active");
-        $(".tabs__item").hide().eq($(this).index()).fadeIn();
-    }).eq(0).addClass("active");
-    $(".tabs__item").eq(0).fadeIn();
+	const swiper = new Swiper('#about .swiper', {
+		loop: true,
+		autoplay: {
+			delay: 5000,
+		},
+		slidesPerView: 1,
+		spaceBetween: 30,
+		simulateTouch: false,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		breakpoints: {
+			600: {
+				slidesPerView: 2,
+				spaceBetween: 30,
+			},
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+
+			1224: {
+				slidesPerView: 4,
+				spaceBetween: 30,
+			},
+		},
+	});
+
+	//Clients swiper
+	const clients_swiper = new Swiper('.clients__swiper', {
+		loop: true,
+		autoplay: {
+			delay: 5000,
+		},
+		slidesPerView: 1,
+		spaceBetween: 30,
+		autoHeight: true,
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+			autoheight: true,
+		},
+	});
+
+	//Tabs
+	function tabs(
+		tabsSelector,
+		tabsContentSelector,
+		tabsParentSelector,
+		activeClass
+	) {
+		const tabs = document.querySelectorAll(tabsSelector),
+			tabsContent = document.querySelectorAll(tabsContentSelector),
+			tabsParent = document.querySelector(tabsParentSelector);
+
+		function hideTabContent() {
+			tabsContent.forEach((tab) => {
+				tab.classList.remove('active', 'fade');
+			});
+
+			tabs.forEach((tab) => {
+				tab.classList.remove(activeClass);
+			});
+		}
+
+		function showTabContent(i = 0) {
+			tabsContent[i].classList.add('active', 'fade');
+			tabs[i].classList.add(activeClass);
+		}
+
+		hideTabContent();
+		showTabContent();
+
+		tabsParent.addEventListener('click', (event) => {
+			const target = event.target;
+
+			if (target && target.classList.contains(tabsSelector.slice(1))) {
+				tabs.forEach((tab, i) => {
+					if (tab === target) {
+						hideTabContent();
+						showTabContent(i);
+					}
+				});
+			}
+		});
+	}
+
+	tabs('.tabs__button', '.tabs__item', '.tabs__buttons', 'active');
 });
-
-$(window).scroll(function () {
-    const headerHeight = $('.wrapper-fixed').outerHeight();
-    const bannerHeight = $('.header').outerHeight();
-    if($(window).scrollTop() >= (bannerHeight - headerHeight)) {
-        $('.wrapper-fixed').addClass('sticky');
-        console.log('hello');
-    }
-    else {
-        $('.wrapper-fixed').removeClass('sticky');
-    }
-})
